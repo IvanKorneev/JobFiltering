286f5ede7af58ee7e0e704db549c5db38bc9b4ed
IvanKorneev <ivan.kornieiev@gmail.com>
2022-06-29 12:16:28 +0300

    add component Stack

---
+++ src/components/Stack/Stack.js
@@ -0,0 +1,9 @@
+const Stack = ({children, pos}) => (
+    <div className='stack' style={{
+        justifyContent: pos === 'center' ? 'center' : `flex-${pos}`
+    }}>
+        {children}
+    </div>
+);
+
+export default Stack;
