4e999a43e5d56d90b4e5c6241306952f3668f9c8
IvanKorneev <ivan.kornieiev@gmail.com>
2022-06-29 12:14:27 +0300

    add component Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+const Card = ({children, isFeatured, className}) => {
+    return (
+        <div
+            className={
+                `card${isFeatured ? ' card--featured' : ''}${className ? ' ' + className : ''}`
+            }
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Card;
