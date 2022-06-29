6ccfa37e5bfd915f1e8519c88126a59b02c2c079
IvanKorneev <ivan.kornieiev@gmail.com>
2022-06-29 12:12:57 +0300

    add component Badge

---
+++ src/components/Badge/Badge.js
@@ -0,0 +1,21 @@
+import {ReactComponent as Remove} from '../assets/images/icon-remove.svg';
+const Badge = ({
+                   variant = 'basic',
+                   colorScheme = 'light',
+                   children,
+                   onClear,
+                   onClick,
+               }) => (
+    <div className={`badge badge--${variant} badge--${colorScheme}`} onClick={onClick}>
+    <span>
+      {children}
+    </span>
+        {variant === 'clearable' && (
+            <div className='badge-remover' onClick={onClear}>
+                <Remove />
+            </div>
+        )}
+    </div>
+);
+
+export default Badge;
