5a820468d2163574490a27b9016b01f312aabac4
IvanKorneev <ivan.kornieiev@gmail.com>
2022-06-29 11:48:20 +0300

    add component FilterPanel

---
+++ src/components/FilterPanel/FilterPanel.js
@@ -0,0 +1,17 @@
+const FilterPanel = () => {
+    return (
+        <Card className="filter-panel">
+            <div className="filter-panel-wrapper">
+                <Stack>
+                    <Badge variant="clearable">Frontend</Badge>
+                    <Badge variant="clearable">Backend</Badge>
+                    <Badge variant="clearable">React</Badge>
+                </Stack>
+
+                <button className='link'>Clear</button>
+            </div>
+        </Card>
+    )
+}
+
+export default FilterPanel;
