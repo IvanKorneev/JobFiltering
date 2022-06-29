b3445607bd54c7a44685bd563992552fa7edfe7c
IvanKorneev <ivan.kornieiev@gmail.com>
2022-06-29 15:05:44 +0300

    add filter-reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import {positionReducers} from "./positions/position-reducers";
+import {filterReducer} from "./filters/filter-reducers";
 
 export const rootReducer = combineReducers({
     positions: positionReducers,
+    filters:filterReducer
 });
